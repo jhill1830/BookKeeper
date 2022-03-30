be0b1283e8b7b200cb2c7c722b9f1671078750e4
James <36040856+jhill1830@users.noreply.github.com>
2022-03-30 22:56:07 +1000

    testing web scraping stuff

---
+++ bookKeeper.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import urllib.request
+import urllib.parse
+import urllib.error
+from bs4 import BeautifulSoup
+
+# ? Site scrubber, to write online novels to notepad/word/whatever for offline reading
+# ? It goes over each site page chapter and creates new chapters, and continues to next webpage(aka next chapter) until there're no more chapters
+# ? Maybe add an update feature if the book isn't finished, so it captures new chapters and send a notification when a new chapter is out and written
+# ? Potentially, also have it work for graphic novels to save the pictures
+# ? Could have a shell script that runs the program each day/week, with arguments that define the book and/or website
+# ? Maybe choose which file to write to using the arguments in the shell script
+# ? Must keep track of which chapter it is up to (use JSON file?), so as to only add new chapters to the offline file, and not duplicate already read and written chapters.
+# ? Some way of roughly evaluating how big the file will be once the program has file has been written. (Prompt user?) Potentially no as this may get in the way of automating in the shell script
+
+#string = "this is a test string"
+# def scrub(str):
+#    print(str)
+#    return
+
+
+# scrub(string)
+# print(string)
+
+url = 'https://novelfull.com/reverend-insanity/chapter-323.html'
+testUrl = 'https://beautiful-soup-4.readthedocs.io/en/latest/#making-the-soup'
+
+fhand = urllib.request.urlopen(testUrl).read()
+soup = BeautifulSoup(fhand, 'html.parser')
+# for line in fhand:
+#    print(line.decode().strip())
+
+pTags = soup('h1')          # finds only selected elements eg. 'h1'
+for tags in pTags:          # iterate line by line through site
+    print(tags.get_text())  # print only the text of the selected element
+
+# print(pTags.__sizeof__())
+# print(fhand.read())
