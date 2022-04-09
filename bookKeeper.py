48d1d4b2ebc5fbde170afcc76c548fa1df085cbc
James <36040856+jhill1830@users.noreply.github.com>
2022-04-09 20:23:34 +1000

    Added previewChap variable and it's utilization in nextChap func. nextChap function will write the last chapter in the serie

--- bookKeeper.py
+++ bookKeeper.py
@@ -18,13 +18,11 @@ from bs4 import BeautifulSoup
 # ? Maybe choose which file to write to using the arguments in the shell script
 # ? Must keep track of which chapter it is up to (use JSON file?), so as to only add new chapters to the offline file, and not duplicate already read and written chapters.
 # ? Some way of roughly evaluating how big the file will be once the program has file has been written. (Prompt user?) Potentially no as this may get in the way of automating in the shell script
-# TODO might have to write something to automate the chapter names if that's part of the url in some chapters
 # TODO sort the library JSON file books alphabetically
-# TODO potentially rewrite the manner in which it goes to next chapter by looking for an <a> tag that includes text "Next Chapter". If it does, then copy page and then click link to next page.  Stop the program if there is no "Next Chapter" <a> tag.
 
 # ? ----------------- INPUTS -----------------
-# ! variable 'url' below won't work as the website itself is blocking the request.  Figure out workaround, once a basic webscraper is working on sites that do allow it
-#       # Use shell argument to define url
+
+#       # NOTE Could use shell argument to define url
 url = 'https://bestlightnovel.com/novel_888108451/chapter_'
 # testUrl = 'https://beautiful-soup-4.readthedocs.io/en/latest/#making-the-soup'
 testUrl = 'https://bestlightnovel.com/novel_888108451/chapter_2334'
@@ -35,21 +33,23 @@ bookTitle = sys.argv[1]     # Use shell argument 1
 bookFile = bookTitle + '.txt'
 libraryJson = 'library.json'
 chaptersNum = sys.argv[2]   # Use shell argument 2. # of chapters to read up to
+
+# Check for specificity of preview chapter in shell statement
+try:
+    previewChap = sys.argv[3]
+except:
+    previewChap = False
+
 print(bookTitle)
+
 #
 #
 #
-#
-#
-#
-#
-#
+
 
 # ? ----------------- Send Request -----------------
 
-
 # TODO maybe rewrite the sendRequest function to check for valid url address/ address that has a proper book in it
-
 
 def sendReq(site, tag):  # sendRequest function to connect and parse url which returns desired tag
     data = json.load(open(libraryJson, 'r'))
@@ -65,21 +65,16 @@ def sendReq(site, tag):  # sendRequest function to connect and parse url which r
 #
 #
 #
-#
-#
-#
-#
-#
+
 
 # ? ----------------- File/URL Reading -----------------
-
 
 def scrub(site, bookfile):  # scrubbing function
     data = json.load(open(libraryJson, 'r'))
     # If up to date with specified chapter number
     if data['books'][bookTitle]['chapter'] <= int(chaptersNum):
         # IF book exists and no new chapter to write: RETURN/close program
-        if nextChap(site):
+        if nextChap(site, bookfile):
             # TODO write if statements for book existence etc
 
             # # Could potentially use scrub as a recursive function where it increments the url number until it somes up with a blank site/placeholder site. eg replace the chapter number in url
@@ -112,10 +107,6 @@ def scrub(site, bookfile):  # scrubbing function
 #
 #
 #
-#
-#
-#
-#
 
 
 # ? ----------------- WRITE TO FILE -----------------
@@ -139,10 +130,6 @@ def writeBook(bookfile, tag, jsonFile):  # write to file function.
         file.write('\n'*2)
     os.chdir(r'C:\Users\James Hillman\Documents\Personal Repos\BookKeeper')
 
-#
-#
-#
-#
 #
 #
 #
@@ -171,13 +158,7 @@ def updateChapter(book, jsonFile):    # update file's chapter + 1
 #
 #
 #
-#
-#
-#
-#
-#
-#
-#
+
 
 # ? ----------------- UPDATE LIBRARY -----------------
 
@@ -185,7 +166,7 @@ def updateChapter(book, jsonFile):    # update file's chapter + 1
 # BUG: this functions will update the json file in the wrong format if used on a pre-existing key/entry.  But works properly if sed to create a new entry
 def updateLibrary(book, jsonFile):  # Dunno if necessary. Might use a book list to reference so that if the book exists already, then append('a') to the corresponding file, otherwise, write('w') to new file
     data = json.load(open(jsonFile, 'r'))
-    data['books'][book] = {'title': book, 'chapter': 1}
+    data['books'][book] = {'title': book, 'chapter': 1, 'url': url}
 
     with open(jsonFile, 'r+') as updateFile:
         json.dump(data, updateFile, indent=4)
@@ -197,20 +178,16 @@ def updateLibrary(book, jsonFile):  # Dunno if necessary. Might use a book list 
 #
 #
 #
-#
-#
-#
-#
-#
+
 
 # ? ----------------- NEXT CHAPTER -----------------
 
 
 # NOTE: This will cause the last chapter in the book to not be written. # TODO: Might have to add if statement to check if it is the last chapter in the series. Use specified chapter num.
-# NOTE: This might also be an issue as the book will always be a chapter behind the most recent if the site doesn't do preview pages for the upcoming, incompleted chapters. Potentially write the file writing and sjson updating into this function so it write the page before it returns false.  That way it still writes the last url(AKA final chapter in series), as well as the most recently updated chapter if it's not complete yet. NOTE: this will only be viable if there is no preview pages for the next chapter, cause otherwise it will update the chapter number in the json when it has only written the preview for that chapter. NOTE: maybe check for a "Prev Chapter" <a> tag, to at least validate that it's a proper novel chapter url and doesn't write rnadom stuff from a chapter that doesn't exist(Still a problem with preview chapters.  Maybe have an arg specifying if the site has previews or not)
+# NOTE: This might also be an issue as the book will always be a chapter behind the most recent if the site doesn't do preview pages for the upcoming, incompleted chapters. Potentially write the file writing and json updating into this function so it write the page before it returns false.  That way it still writes the last url(AKA final chapter in series), as well as the most recently updated chapter if it's not complete yet. NOTE: this will only be viable if there is no preview pages for the next chapter, cause otherwise it will update the chapter number in the json when it has only written the preview for that chapter. NOTE: maybe check for a "Prev Chapter" <a> tag, to at least validate that it's a proper novel chapter url and doesn't write rnadom stuff from a chapter that doesn't exist(Still a problem with preview chapters.  Maybe have an arg specifying if the site has previews or not)
 
 # Uses existence of <a> tag with 'Next Chapter' text, to check if there is a valid next chapter url to go to.
-def nextChap(site):
+def nextChap(site, bookfile):
     aTag = sendReq(site, 'a')
     for tag in aTag:
         try:
@@ -219,19 +196,39 @@ def nextChap(site):
                 return True
         except:
             continue
+
     # if yes preview: just return false
+    if previewChap:
+        data = json.load(open(libraryJson, 'r'))
+        # write the last chapter if it's the last one specified
+        if data['books'][bookTitle]['chapter'] == int(chaptersNum):
+            writeBook(bookfile, sendReq(site, 'p'), libraryJson)
+            updateChapter(bookTitle, libraryJson)
+            print('Last Chapter')
+            return False
+        print("No New Chapter")
+        return False
+
     # if no preview and if "Prev Chapter" <a> tag: write current page, then return false
-    print("No New Chapter")
-    return False
+    if previewChap == False:
+        for tag in aTag:
+            try:
+                line = tag.get_text()
+                if line == 'PREV CHAPTER':
+                    writeBook(bookfile, sendReq(site, 'p'), libraryJson)
+                    updateChapter(bookTitle, libraryJson)
+                    print('Most Recent Chapter')
+                    return False
+            except:
+                continue
+        print("No New Chapter")
+        return False
+
 
 #
 #
 #
-#
-#
-#
-#
-#
+
 
 # ? ----------------- SEND NOTIFICATION -----------------
 
